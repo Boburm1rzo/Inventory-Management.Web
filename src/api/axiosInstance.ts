b81001390fdf16821b58741cd50681aa71b20fe7
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-09 16:40:52 +0500

    update

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken, removeToken } from "../utils/token";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
